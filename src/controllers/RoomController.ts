f3fd455353d22dfa32a613e8b1f07ea92e5d1199
Icegreeen <flavioaquila16@gmail.com>
2023-08-15 15:54:47 -0300

    added videoController

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { roomRepository } from "../repositories/roomRepository";
+import { videoRepository } from "../repositories/videoRepository";
 
 export class RoomController {
     async create(req: Request, res: Response) {
@@ -10,6 +11,32 @@ export class RoomController {
             await roomRepository.save(newRoom)
 
             return res.status(201).json(newRoom);
+        } catch(error) {
+            console.log(error)
+            return res.status(500).json({message: 'Internal server error'})
+        }
+    }
+
+    async createVideo(req: Request, res: Response) {
+        const { title, url, room_id } = req.body
+        const { idRoom } = req.params
+
+        try {
+            const room = await roomRepository.findOneBy({id: Number(idRoom)})
+            
+            if(!room) {
+                return res.status(404).json({message: 'Aula não existe!'})
+            }
+
+            const newVideo = videoRepository.create({
+                title, 
+                url,
+                room
+            })
+
+            await videoRepository.save(newVideo);
+
+            return res.status(201).json(newVideo)
         } catch(error) {
             console.log(error)
             return res.status(500).json({message: 'Internal server error'})
